8e4ceba7762df9efec53374d828fcfec817b1e52
Satyam Gupta <guptasatyam1709@gmail.com>
2025-04-16 17:02:17 +0530

    tabs added

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import {Link} from 'expo-router';
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to ReState</Text>
+     </View>
+  );
+}
--- app/signin.tsx
+++ app/signin.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { login } from '@/lib/appwrite';
 import images from '@/constants/images';
 import icons from '@/constants/icons';
+import { useGlobalContext } from '@/lib/global-provider';
+import {Redirect} from 'expo-router';
 const SignIn = () => {
-    const handleLogin =()=>{
-        console.log('Login button clicked')
+    const {refetch, loading, isLogged}= useGlobalContext();
+    if(!loading && isLogged) return <Redirect href="/"/> 
+    const handleLogin = async()=>{
+        const result = await login();
+        if (result) {
+          refetch();
+        } else {
+        Alert.alert("Error", "Failed to login");
+        }
     }
     return (
         <SafeAreaView className='bg-white h-full'>
